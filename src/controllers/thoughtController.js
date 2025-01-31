c96d7c75b5bd35e47aba76b86df54c8c78e82d9b
Baileigh Remy <baileigh.remy@gmail.com>
2025-01-30 22:00:48 -0600

    Added user and thought controller

---
+++ src/controllers/thoughtController.js
@@ -0,0 +1,78 @@
+import Thought from '../models/Thought.js';
+import { Request, Response } from 'express';
+
+export const getThoughts = async (_req, res) => {
+    try {
+        const thoughts = await Thought.find({});
+        res.json(thoughts);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const getThoughtById = async (req, res) => {
+    try {
+        const thought = await Thought.findById(req.params.id);
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const createThought = async (req, res) => {
+    try {
+        const thought = await Thought.create(req.body);
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const updateThought = async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true });    
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }   
+};
+
+export const deleteThought = async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndDelete(req.params.id);
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const addReaction = async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, { $addToSet: { reactions: req.body } }, { new: true });
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const deleteReaction = async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { new: true });
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+
+// export const deleteFriend = async (req, res) => {
+//     try {
+//         const user = await User.findByIdAndUpdate(req.params.userId, { $pull: { friends: req.params.friendId } }, { new: true });    
+//         res.json(user);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+//     };
+
+
+
---
+++ src/controllers/userController.js
@@ -0,0 +1,65 @@
+import User from '../models/User';
+import { Request, Response } from 'express';
+
+export const getAllUsers = async (_req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    };
+
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    };
+
+export const createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    };
+
+export const updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    };
+
+export const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    };
+
+export const addFriend = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.userId, { $addToSet: { friends: req.params.friendId } }, { new: true });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    };
+
+export const deleteFriend = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.userId, { $pull: { friends: req.params.friendId } }, { new: true });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);  
+    }
+    };
