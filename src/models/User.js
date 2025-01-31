e178ff88b54f1365e985703b0dbe7acae44053ba
Baileigh Remy <baileigh.remy@gmail.com>
2025-01-30 21:26:53 -0600

    Added file layout and User.js model

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+export default User;
